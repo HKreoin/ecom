0f0f50486c7aeb0e57b273761683f6371fffc631
Ilshat Khabirov <khabdev@yandex.ru>
2025-02-16 19:25:54 +0300

    add flowbite and Admin dashboard vue views

---
+++ resources/js/Pages/Admin/Components/AdminLayout.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { onMounted } from 'vue';
+import { initFlowbite } from 'flowbite';
+import Navbar from './Navbar.vue';
+import Sidebar from './Sidebar.vue';
+
+onMounted(() => {
+    initFlowbite();
+});
+</script>
+
+<template>
+    <div class="bg-gray-50 antialiased dark:bg-gray-900">
+        <!-- Navbar -->
+        <Navbar />
+        <!-- Sidebar -->
+        <Sidebar />
+        <!-- Main -->
+        <slot />
+    </div>
+</template>
--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -1,9 +1,64 @@
 <script setup>
+import AdminLayout from './Components/AdminLayout.vue';
+
 defineProps({
     admin: String,
 });
 </script>
 
 <template>
-    <h1>{{ admin }} dashboard</h1>
+    <AdminLayout>
+        <main class="h-auto p-4 pt-20 md:ml-64">
+            <div
+                class="mb-4 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4"
+            >
+                <div
+                    class="h-32 rounded-lg border-2 border-dashed border-gray-300 md:h-64 dark:border-gray-600"
+                ></div>
+                <div
+                    class="h-32 rounded-lg border-2 border-dashed border-gray-300 md:h-64 dark:border-gray-600"
+                ></div>
+                <div
+                    class="h-32 rounded-lg border-2 border-dashed border-gray-300 md:h-64 dark:border-gray-600"
+                ></div>
+                <div
+                    class="h-32 rounded-lg border-2 border-dashed border-gray-300 md:h-64 dark:border-gray-600"
+                ></div>
+            </div>
+            <div
+                class="mb-4 h-96 rounded-lg border-2 border-dashed border-gray-300 dark:border-gray-600"
+            ></div>
+            <div class="mb-4 grid grid-cols-2 gap-4">
+                <div
+                    class="h-48 rounded-lg border-2 border-dashed border-gray-300 md:h-72 dark:border-gray-600"
+                ></div>
+                <div
+                    class="h-48 rounded-lg border-2 border-dashed border-gray-300 md:h-72 dark:border-gray-600"
+                ></div>
+                <div
+                    class="h-48 rounded-lg border-2 border-dashed border-gray-300 md:h-72 dark:border-gray-600"
+                ></div>
+                <div
+                    class="h-48 rounded-lg border-2 border-dashed border-gray-300 md:h-72 dark:border-gray-600"
+                ></div>
+            </div>
+            <div
+                class="mb-4 h-96 rounded-lg border-2 border-dashed border-gray-300 dark:border-gray-600"
+            ></div>
+            <div class="grid grid-cols-2 gap-4">
+                <div
+                    class="h-48 rounded-lg border-2 border-dashed border-gray-300 md:h-72 dark:border-gray-600"
+                ></div>
+                <div
+                    class="h-48 rounded-lg border-2 border-dashed border-gray-300 md:h-72 dark:border-gray-600"
+                ></div>
+                <div
+                    class="h-48 rounded-lg border-2 border-dashed border-gray-300 md:h-72 dark:border-gray-600"
+                ></div>
+                <div
+                    class="h-48 rounded-lg border-2 border-dashed border-gray-300 md:h-72 dark:border-gray-600"
+                ></div>
+            </div>
+        </main>
+    </AdminLayout>
 </template>
