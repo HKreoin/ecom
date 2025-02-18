e42a29bc04d11caa9bea91bc8e4a41ae15871a74
Ilshat Khabirov <khabdev@yandex.ru>
2025-02-18 13:14:36 +0300

    create Index.vue for products

---
+++ resources/js/Pages/Admin/Product/Index.vue
@@ -0,0 +1,14 @@
+<script setup>
+import AdminLayout from '../Components/AdminLayout.vue';
+import ProductList from './ProductList.vue';
+
+defineProps({
+    admin: String,
+});
+</script>
+
+<template>
+    <AdminLayout>
+    <ProductList></ProductList>
+    </AdminLayout>
+</template>
